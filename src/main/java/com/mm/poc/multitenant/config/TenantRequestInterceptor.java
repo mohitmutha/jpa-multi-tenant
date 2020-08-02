8193aa59f108b5a1e14ec2207bde320b3e63d855
Mohit <mohitmutha@gmail.com>
2020-08-02 22:58:17 +0530

    Log statements

--- src/main/java/com/mm/poc/multitenant/config/TenantRequestInterceptor.java
+++ src/main/java/com/mm/poc/multitenant/config/TenantRequestInterceptor.java
@@ -1,5 +1,7 @@
 package com.mm.poc.multitenant.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -9,15 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 
 @Component
 public class TenantRequestInterceptor extends HandlerInterceptorAdapter {
+  private static final Logger logger = LoggerFactory.getLogger(TenantRequestInterceptor.class);
   @Override
   public boolean preHandle(HttpServletRequest request,
                            HttpServletResponse response, Object object) throws Exception {
-    System.out.println("In preHandle we are Intercepting the Request");
-    System.out.println("____________________________________________");
+    logger.debug("In preHandle we are Intercepting the Request");
     String requestURI = request.getRequestURI();
     String tenantID = request.getHeader("X-TenantID");
-    System.out.println("RequestURI::" + requestURI +" || Search for X-TenantID  :: " + tenantID);
-    System.out.println("____________________________________________");
+    logger.debug("RequestURI::" + requestURI +" || Search for X-TenantID  :: " + tenantID);
     if (tenantID == null) {
       response.getWriter().write("X-TenantID not present in the Request Header");
       response.setStatus(400);
