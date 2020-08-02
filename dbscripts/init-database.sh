f6b777b1044ae807a3ea886f635752ff72fa1ba6
Mohit <mohitmutha@gmail.com>
2020-08-02 22:50:39 +0530

    Add sequences

--- dbscripts/init-database.sh
+++ dbscripts/init-database.sh
@@ -30,15 +30,16 @@ psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-EOSQL
   create table employee(id BIGSERIAL PRIMARY KEY, name varchar(200));
   GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO appuser;
   GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO appuser;
-  
+
   --For schema strategy
   \c postgres
   create schema tenant1;
   create table tenant1.employee(id BIGSERIAL PRIMARY KEY, name varchar(200));
   GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA tenant1 TO appuser;
+  GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA tenant1 TO appuser;
 
   create schema tenant2;
   create table tenant2.employee(id BIGSERIAL PRIMARY KEY, name varchar(200));
   GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA tenant2 TO appuser;
-
+  GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA tenant2 TO appuser;
 EOSQL
